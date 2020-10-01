e3a57c148d2e7dfc384e433f1d83cd6832ca9686
aditya-jangid <jangid.aditya25@gmail.com>
2020-10-01 20:20:08 +0530

    Added Dynamic Charts

--- src/app/mastercard/mastercard.component.ts
+++ src/app/mastercard/mastercard.component.ts
@@ -4,7 +4,6 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { AppComponent } from '../app.component';
 import { data } from 'jquery';
 import { DatePipe } from '@angular/common';
-import { arrADBE , arrMA} from '../stock';
 
 @Component({
   selector: 'app-mastercard',
@@ -15,8 +14,8 @@ export class mastercardComponent implements OnInit {
   retrievedData;
   currPath;
 
-  arrADBE: any[];
-  arrMA: any[];
+  // arrADBE: any[];
+  // arrMA: any[];
   view: any[] = [700, 400];
 
   // options
@@ -30,6 +29,7 @@ export class mastercardComponent implements OnInit {
   xAxisLabel: string = 'Date';
   yAxisLabel: string = 'Price';
   timeline: boolean = true;
+  autoScale: boolean = true;
 
 
   colorScheme = {
@@ -40,29 +40,25 @@ export class mastercardComponent implements OnInit {
     private route: ActivatedRoute,
     private AppComponent: AppComponent,
     private router: Router,
-    public datepipe: DatePipe 
+    public datepipe: DatePipe
   ) {
-    Object.assign(this, { arrADBE , arrMA });
+
   }
 
   ngOnInit() {
-    //subscribe into retrievedData
-    this.AppComponent.getData().subscribe(data => this.retrievedData = data);
 
     //get active route
     this.currPath = this.router.url.slice(1);
     console.log(this.currPath);
 
-  }
-  onSelect(data): void {
-    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
-  }
+    fetch(`https://stock-dash-backend.herokuapp.com/api/chartDataby/${this.currPath}`)
+      .then(res => res.json())
+      .then((arrCHART) => {
+        // console.log('Output: ', out);
+        Object.assign(this, { arrCHART });
+      }).catch(err => console.error(err));
 
-  onActivate(data): void {
-    console.log('Activate', JSON.parse(JSON.stringify(data)));
-  }
-
-  onDeactivate(data): void {
-    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
+    //subscribe into retrievedData
+    this.AppComponent.getData().subscribe(data => this.retrievedData = data);
   }
 }
