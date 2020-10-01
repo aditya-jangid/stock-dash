b97241ef2841b1be5cfd4e9bdc92a0938f775f2e
aditya-jangid <jangid.aditya25@gmail.com>
2020-10-01 21:57:48 +0530

    bug fixes

--- src/app/mastercard/mastercard.component.ts
+++ src/app/mastercard/mastercard.component.ts
@@ -1,64 +1,67 @@
 import { Component, OnInit } from '@angular/core';
 import * as Chartist from 'chartist';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { Router } from '@angular/router';
 import { AppComponent } from '../app.component';
 import { data } from 'jquery';
 import { DatePipe } from '@angular/common';
 
 @Component({
-  selector: 'app-mastercard',
-  templateUrl: './mastercard.component.html',
-  styleUrls: ['./mastercard.component.css']
+	selector: 'app-mastercard',
+	templateUrl: './mastercard.component.html',
+	styleUrls: ['./mastercard.component.css']
 })
 export class mastercardComponent implements OnInit {
-  retrievedData;
-  currPath;
+	retrievedData;
+	currPath;
 
-  // arrADBE: any[];
-  // arrMA: any[];
-  view: any[] = [700, 400];
+	// arrADBE: any[];
+	// arrMA: any[];
+	view: any[];
 
-  // options
-  legend: boolean = true;
-  showLabels: boolean = true;
-  animations: boolean = true;
-  xAxis: boolean = true;
-  yAxis: boolean = true;
-  showYAxisLabel: boolean = true;
-  showXAxisLabel: boolean = true;
-  xAxisLabel: string = 'Date';
-  yAxisLabel: string = 'Price';
-  timeline: boolean = true;
-  autoScale: boolean = true;
+	// options
+	legend: boolean = true;
+	showLabels: boolean = true;
+	animations: boolean = true;
+	xAxis: boolean = true;
+	yAxis: boolean = true;
+	showYAxisLabel: boolean = true;
+	showXAxisLabel: boolean = true;
+	xAxisLabel: string = 'Date';
+	yAxisLabel: string = 'Price($)';
+	timeline: boolean = true;
+	autoScale: boolean = true;
 
 
-  colorScheme = {
-    domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5']
-  };
+	colorScheme = {
+		domain: ['#d81b60', '#43a047']
+	};
 
-  constructor(
-    private route: ActivatedRoute,
-    private AppComponent: AppComponent,
-    private router: Router,
-    public datepipe: DatePipe
-  ) {
+	constructor(
+		private AppComponent: AppComponent,
+		private router: Router,
+		public datepipe: DatePipe
+	) {
 
-  }
+	}
 
-  ngOnInit() {
+	ngOnInit() {
 
-    //get active route
-    this.currPath = this.router.url.slice(1);
-    console.log(this.currPath);
+		//get active route
+		this.currPath = this.router.url.slice(1);
+		console.log(this.currPath);
 
-    fetch(`https://stock-dash-backend.herokuapp.com/api/chartDataby/${this.currPath}`)
-      .then(res => res.json())
-      .then((arrCHART) => {
-        // console.log('Output: ', out);
-        Object.assign(this, { arrCHART });
-      }).catch(err => console.error(err));
+		if ($(window).width() > 991) {
+			this.view = [700, 400];
+		}
 
-    //subscribe into retrievedData
-    this.AppComponent.getData().subscribe(data => this.retrievedData = data);
-  }
+		fetch(`https://stock-dash-backend.herokuapp.com/api/chartDataby/${this.currPath}`)
+			.then(res => res.json())
+			.then((arrCHART) => {
+				// console.log('Output: ', out);
+				Object.assign(this, { arrCHART });
+			}).catch(err => console.error(err));
+
+		//subscribe into retrievedData
+		this.AppComponent.getData().subscribe(data => this.retrievedData = data);
+	}
 }
